c46368a553c0cf23921ac06e22a831972443ff23
cowton0627 <83654992+cowton0627@users.noreply.github.com>
2026-05-14 17:13:22 +0800

    feat(notifications): request authorization and show banner in foreground
    沒先 requestAuthorization,scheduleSuccessNotification add 不會跳通知;
    前景時又沒 willPresent delegate,即使授權過 banner 也不顯示。
    加上授權請求 + UNUserNotificationCenterDelegate,讓送單成功的本地推播實際會出現。

--- NewOrderOoO/AppDelegate.swift
+++ NewOrderOoO/AppDelegate.swift
@@ -4,6 +4,7 @@
 //
 
 import UIKit
+import UserNotifications
 import FirebaseCore
 import FirebaseAuth
 
@@ -27,6 +28,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
 
+        // 本地推播：請求授權 + 設 delegate，讓 app 在前景時也能顯示橫幅
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { _, _ in }
+
         return true
     }
 
@@ -45,4 +51,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
 }
